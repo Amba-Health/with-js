a2c12566b9a05a287453b6025f7dda1fc12a1d83
Romaric <romaric@cookieshq.co.uk>
2020-02-19 18:59:55 +0000

    Document the reason for the separate 'export.js' file

--- src/export.js
+++ src/export.js
@@ -1,3 +1,10 @@
+/**
+ * A thin module to create a `default` export
+ * of the `withJS` function and add the other
+ * named exports to it, for bundling the IIFE
+ * and CommonJS exports
+ */
+
 import {
   withJS,
   SELECTOR,
